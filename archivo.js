5239041f833af652165fa002cf8385e4e4287c50
Paulazarte <paulazarte1997@gmail.com>
2020-09-07 23:57:21 -0300

    empecé con las listas de películas y el segundo navbar

---
+++ archivo.js
@@ -0,0 +1,8 @@
+// var prueba = $('.prueba');
+// prueba.on('mouseenter focus', function(){
+//     prueba.get(0).play()
+// })
+
+// prueba.on('mouseout blur', function(){
+//     prueba.get(0).pause()
+// })
