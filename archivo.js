193c675ced81e4f651c6b7edabbe4ac79f2aeb8c
Paulazarte <paulazarte1997@gmail.com>
2020-09-10 15:56:32 -0300

    nuevos modificaciones en el DOM

--- archivo.js
+++ archivo.js
@@ -1,3 +1,19 @@
+// var figure = $(".video");
+// var vid = figure.find("video");
+
+// [].forEach.call(figure, function (item,index) {
+//     item.addEventListener('mouseover', hoverVideo.bind(item,index), false);
+//     item.addEventListener('mouseout', hideVideo.bind(item,index), false);
+// });
+
+// function hoverVideo(index, e) {
+//     vid[index].play(); 
+// }
+
+// function hideVideo(index, e) {
+//     vid[index].pause(); 
+// }
+
 // var prueba = $('.prueba');
 // prueba.on('mouseenter focus', function(){
 //     prueba.get(0).play()
@@ -6,3 +22,11 @@
 // prueba.on('mouseout blur', function(){
 //     prueba.get(0).pause()
 // })
+
+
+// var element = document.getElementById("div1");
+// element.classList.add("otherclass"); 
+// element.classList.remove("otherclass");
+
+// document.getElementById("my-element").remove();
+// document.getElementById("my-element").add();
