1f7588c8d766f68611dd1389843209ae253595f7
Paulazarte <paulazarte1997@gmail.com>
2020-09-28 01:48:05 -0300

    Terminando la ventana modal

--- js/adm.js
+++ js/adm.js
@@ -1,8 +1,41 @@
-var btnAbrirPopup = document.getElementById('boton-nuevapeli'),
-    overlay = document.getElementById('overlay'),
-    popup = document.getElementById('popup'),
-    btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+// Get the modal
+var modal = document.getElementById("myModal");
 
-btnAbrirPopup.addEventListener('click',function(){
-    overlay.classList.add('active');
-});
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+//Formulario
+
+var inputs = document.getElementsByClassName ("formulario__input");
+for (var i =0; i <inputs.length; i++) {
+inputs [i].addEventListener ("keyup", function(){
+if(this.value.length>=1) {
+    this.nextElementSibling.classList.add("fijar");
+} else {
+    this.nextElementSibling.classList.remove("fijar");
+
+}
+
+})
+
+}
