f3cf5e5c0f5bfc6feefc84cf143c0295c149c591
Paulazarte <paulazarte1997@gmail.com>
2020-10-08 01:19:23 -0300

    index de administración terminado

--- js/adm.js
+++ js/adm.js
@@ -48,45 +48,6 @@ for (var i = 0; i < inputs.length; i++) {
 
 }
 
-//AGREFAR DATOS A LA TABLA
-// class Datos{
-//   constructor(codigo, nombre,descripcion){
-//     this._codigo = codigo;
-//     this._nombre = nombre;
-//     this._descripcion = descripcion;
-//   }
-// }
-
-// class Visu {
-//   agregarInfo(datos){
-//     const tabla = document.getElementById('tabla');
-//     const elementos = document.createElement("tr");
-//     elementos.innerHTML = `
-
-//         <td>${datos._codigo}</td> 
-//         <td>${datos._nombre}</td> 
-//         <td>${datos._descripcion}</td> 
-//         <td></td> 
-//         <td><input type="checkbox"></td> 
-//         <td>&nbsp;<i class="far fa-trash-alt"></i>&nbsp;&nbsp;&nbsp;&nbsp;<i class="far fa-edit"></i>&nbsp;&nbsp;&nbsp;<i class="fas fa-star"></i></td>
-//     `;
-//     tabla.appendChild(elementos);
-//   }
-// }
-// const evento = document
-//   .getElementById('formulario')
-//   .addEventListener('submit', function(e){
-//     const codigo = document.getElementById('codigo').value;
-//     const nombre = document.getElementById('nombre').value;
-//     const descripcion = document.getElementById('descripcion').value;
-
-//     const ponerElementos = new Datos (codigo, nombre,descripcion);
-//     const visu = new Visu();
-//     visu.agregarInfo(ponerElementos);
-
-//     e.preventDefault();
-//   })
-
 
 //LOCALSTORAGE E INGRESO DE DATOS HACIA LA TABLA
 
@@ -158,9 +119,9 @@ const PintarDB = () => {
           Estado - <b>${element.estado}</b>
         </td>  
         <td>
-          <span class="material-icons mr-3">delete</span> 
-          <span class="material-icons mr-3">edit</span>
-          <span class="material-icons mr-3">done_all</span>
+          <span style= "cursor: pointer;" class="material-icons mr-3">delete</span> 
+          <span style= "cursor: pointer;" class="material-icons mr-3">edit</span>
+          <span style= "cursor: pointer;" class="material-icons mr-3">done_all</span>
         </td>
   </tr>`
       }
